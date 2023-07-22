2ad606a501ab78e8bd83da034487413fc3765e7a
galbeiroc <alcresgu_1989@hotmail.com>
2023-07-22 11:25:21 -0500

    add normal js logic to component

--- 03-react-basics/030-starting-setup/src/App.js
+++ 03-react-basics/030-starting-setup/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <ExpensiveItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpensiveItem expense={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+      <ExpensiveItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpensiveItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
       <ExpensiveItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
     </div>
