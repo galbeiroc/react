bf046049cdf35b890ae8e0175d7a99423fcb37e2
galbeiroc <alcresgu_1989@hotmail.com>
2023-07-27 23:43:01 -0500

    handle form submission

--- 05-react-state-with-events/src/components/NewExpense/ExpenseForm.js
+++ 05-react-state-with-events/src/components/NewExpense/ExpenseForm.js
@@ -45,8 +45,19 @@ const ExpenseForm = () => {
     }
   };
 
+  const submitHnadler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+    console.log(expenseData);
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHnadler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
