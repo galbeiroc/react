ea5fa9a37094e5f570dc1e5af2b59059dc446e3c
galbeiroc <alcresgu_1989@hotmail.com>
2023-07-29 00:36:12 -0500

    derived/computed state

--- 05-react-state-with-events/src/components/Expenses/Expenses.js
+++ 05-react-state-with-events/src/components/Expenses/Expenses.js
@@ -8,8 +8,38 @@ import "./Expenses.css";
 const Expenses = (props) => {
   const [filtered, setFiltered] = useState('2020');
 
+  let filterInfoText = '2021, 2022, 2023, 2024 & 2025';
+
+  if (filtered === '2020') {
+    filterInfoText = '2021, 2022, 2023, 2024 & 2025';
+  } else if (filtered === '2021') {
+    filterInfoText = '2020, 2022, 2023, 2024 & 2025';
+  } else if (filtered === '2022') {
+    filterInfoText = '2020, 2021, 2023, 2024 & 2025';
+  } else if (filtered === '2023') {
+    filterInfoText = '2020, 2021, 2022, 2024 & 2025';
+  } else if (filtered === '2024') {
+    filterInfoText = '2020, 2021, 2022, 2023 & 2025';
+  } else {
+    filterInfoText = '2020, 2021, 2022, 2023 & 2024';
+  }
+
+
   const filtereChangeHandler = selectedYear => {
     setFiltered(selectedYear);
+    /* if (selectedYear === '2020') {
+      setFilterInfoText('2021, 2022, 2023, 2024 & 2025');
+    } else if (selectedYear === '2021') {
+      setFilterInfoText('2020, 2022, 2023, 2024 & 2025');
+    } else if (selectedYear === '2022') {
+      setFilterInfoText('2020, 2021, 2023, 2024 & 2025');
+    } else if (selectedYear === '2023') {
+      setFilterInfoText('2020, 2021, 2022, 2024 & 2025');
+    } else if (selectedYear === '2024') {
+      setFilterInfoText('2020, 2021, 2022, 2023 & 2025');
+    } else {
+      setFilterInfoText('2020, 2021, 2022, 2023 & 2025');
+    } */
     console.log('expenses.js', selectedYear);
   }
 
@@ -17,6 +47,7 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter filtered={filtered} onChangeFilter={filtereChangeHandler} />
+        <p>Data for years {filterInfoText} is hidden.</p>
         <ExpensiveItem
           title={props.expenses[0].title}
           amount={props.expenses[0].amount}
