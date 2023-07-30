4a3e62f88fe2c936d45a7fc50c0fdcb3f40b8850
galbeiroc <alcresgu_1989@hotmail.com>
2023-07-30 01:07:35 -0500

    render list with map arr method

---
+++ 06-render-list-&-conditional/src/components/Expenses/Expenses.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Card from "../UI/Card";
+import ExpensiveItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filtered, setFiltered] = useState("2020");
+
+  const filtereChangeHandler = (selectedYear) => {
+    setFiltered(selectedYear);
+    console.log("expenses.js", selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          filtered={filtered}
+          onChangeFilter={filtereChangeHandler}
+        />
+        {props.expenses.map((expense) => (
+          <ExpensiveItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
