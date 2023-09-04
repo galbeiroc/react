c7c1839bd5bafc4ceb544487b1406d935d78046a
galbeiroc <alcresgu_1989@hotmail.com>
2023-09-03 21:09:09 -0500

    add header component

---
+++ 09-the-first-practice-project/react-investment/src/components/Header.js
@@ -0,0 +1,10 @@
+import logo from "../assets/investment-calculator-logo.png";
+
+const Header = () => (
+  <header className="header">
+    <img src={logo} alt="logo" />
+    <h1>Investment Calculator</h1>
+  </header>
+);
+
+export default Header;
