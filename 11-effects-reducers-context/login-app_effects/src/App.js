5e02f5b013dfcaf32adc6037d99b13d1c6ebc3aa
galbeiroc <alcresgu_1989@hotmail.com>
2023-09-25 23:17:23 -0500

    use first way - consumer

--- 11-effects-reducers-context/login-app_effects/src/App.js
+++ 11-effects-reducers-context/login-app_effects/src/App.js
@@ -30,7 +30,7 @@ function App() {
 
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn: false }}
+      value={{ isLoggedIn }}
     >
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
