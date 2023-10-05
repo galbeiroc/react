f2fa22856b832f59c774569e6a47e94f1da8c12b
galbeiroc <alcresgu_1989@hotmail.com>
2023-10-05 01:41:05 -0500

    add meals component

---
+++ 12-the-second-practice-project/food-order/src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        Choose your favorite meal from our board selection of available meals and enjoy a delicious lunch or dinnert home.
+      </p>
+      <p>
+        All our meals are cooked high-quality ingredients, just-in-time and of course by experienced chef!
+      </p>
+    </section>
+  )
+}
+
+export default MealsSummary;
