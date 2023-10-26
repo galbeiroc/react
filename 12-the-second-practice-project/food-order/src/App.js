52792ffa64fa73ef947d632a60b014d91e710675
galbeiroc <alcresgu_1989@hotmail.com>
2023-10-26 00:28:08 -0500

    add cart context

--- 12-the-second-practice-project/food-order/src/App.js
+++ 12-the-second-practice-project/food-order/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import CartProvider from "./context/CartProvider";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
@@ -16,13 +17,13 @@ function App() {
   };
 
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
