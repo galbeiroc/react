7ac2c0e8ab89028a3f0e87360443fee0befc5c76
galbeiroc <alcresgu_1989@hotmail.com>
2023-10-25 23:37:46 -0500

    add modal component

---
+++ 12-the-second-practice-project/food-order/src/components/UI/Modal.js
@@ -0,0 +1,31 @@
+import { createPortal } from "react-dom";
+
+import classes from "./Modal.module.css";
+
+const Backdrop = () => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <>
+      {createPortal(<Backdrop />, portalElement)}
+      {createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
