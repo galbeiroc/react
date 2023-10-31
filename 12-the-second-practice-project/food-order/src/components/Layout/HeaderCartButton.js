60541287f29828a4d120bf023b12c26d483b586b
galbeiroc <alcresgu_1989@hotmail.com>
2023-10-31 17:55:56 -0500

    use usecontext

--- 12-the-second-practice-project/food-order/src/components/Layout/HeaderCartButton.js
+++ 12-the-second-practice-project/food-order/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,24 @@
+import { useContext  } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../context/cart-context';
+
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+    return currNumber + item.amount
+  }, 0)
+
   return (
     <button className={classes.button} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   )
 }
