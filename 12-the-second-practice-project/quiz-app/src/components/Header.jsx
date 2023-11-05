e65b2ec321e8615471a93b46bfc69c560821d2be
galbeiroc <alcresgu_1989@hotmail.com>
2023-11-05 14:41:39 -0500

    add header component

---
+++ 12-the-second-practice-project/quiz-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/quiz-logo.png';
+
+function Header() {
+  return <header>
+    <img src={logo} alt='quiz logo'/>
+    <h1>ReactQuiz</h1>
+  </header>
+}
+
+export default Header;
