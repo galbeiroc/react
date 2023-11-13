f8dbb3aeed877518e95fd2989d59dc91ed14bad8
galbeiroc <alcresgu_1989@hotmail.com>
2023-11-13 00:13:15 -0500

    work with effect and usecallback

--- 12-the-second-practice-project/quiz-app/src/components/Quiz.jsx
+++ 12-the-second-practice-project/quiz-app/src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import quizCompleteImg from "../assets/quiz-complete.png";
 import QUESTIONS from "../question";
@@ -10,9 +10,14 @@ function Quiz() {
   const acitveQuestionIndex = userAnswers.length;
   const quizIsCompleted = acitveQuestionIndex === QUESTIONS.length;
 
-  const handleSelectQuestion = (selectedAnswer) => {
+  const handleSelectQuestion = useCallback((selectedAnswer) => {
     setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
-  };
+  }, []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectQuestion(null),
+    [handleSelectQuestion]
+  );
 
   if (quizIsCompleted) {
     return (
@@ -29,10 +34,7 @@ function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
-        <QuestionTimer
-          timeout={10000}
-          onTimeout={() => handleSelectQuestion(null)}
-        />
+        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
         <h2>{QUESTIONS[acitveQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
